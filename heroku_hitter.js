f504887a3878847dc903d246968d35c69082ae59
Matthew Welke <mattwelke@gmail.com>
2017-02-15 16:18:08 -0500

    init

---
+++ heroku_hitter.js
@@ -0,0 +1,23 @@
+#!/usr/bin/nodejs
+
+const axios = require('axios');
+
+let count = 0;
+const uri = process.argv[2];
+console.log(`Starting with uri = ${uri}.`);
+let timeBefore = undefined;
+let timeAfter = undefined;
+
+function hit() {
+  timeBefore = new Date();
+
+  axios.get(uri)
+    .then(response => {
+      const timeAfter = new Date();
+      console.log(`#${count} - ${response.status} ${response.statusText} response in ${timeAfter - timeBefore} ms`);
+      timeBefore = timeAfter;
+      hit();
+    });
+}
+
+hit();
