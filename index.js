c9f02274e59f7adf84aad2d98c6d3391cfa1f7fe
Matt Welke <matt.welke@groupbyinc.com>
2019-06-05 09:43:23 -0400

    Switch from Date to perf_hooks.

--- index.js
+++ index.js
@@ -1,25 +1,38 @@
 const axios = require('axios');
+const { performance } = require('perf_hooks');
 
 let count = 0;
 let totalDurMs = 0;
 
 const URL = process.argv[2];
-const INTERVAL_MS = process.argv[3];
+const INTERVAL_MS = +process.argv[3];
 
-console.log(`Starting with URL = ${URL}.`);
+console.log(`Starting with:`);
+console.log(`URL = ${URL}`);
+console.log(`Interval (ms) = ${INTERVAL_MS}`);
 
 async function hit() {
-  const t1 = Date.now();
+    try {
+        const t1 = performance.now();
 
-  const response = await axios.get(URL);
+        const response = await axios.get(URL, {
+            headers: {
+                'Host': 'helloworld-go.default.example.com',
+            },
+        });
 
-  const durMs = Date.now() - t1;
+        const durMs = performance.now() - t1;
 
-  totalDurMs += durMs;
+        totalDurMs += durMs;
 
-  count++;
-  
-  console.log(`#${count} - ${response.status} ${response.statusText} response in ${Date.now() - t1} ms. Avg response time = ${(totalDurMs / count).toFixed(0)} ms.`);
-}  
+        count++;
+
+        console.log(`#${count} - ${response.status} ${response.statusText} response in ${durMs.toFixed(0)} ms. Avg response time = ${(totalDurMs / count).toFixed(0)} ms.`);
+    } catch (e) {
+        console.error(`Error:`);
+        console.error(e);
+    }
+
+}
 
 setInterval(hit, INTERVAL_MS);
